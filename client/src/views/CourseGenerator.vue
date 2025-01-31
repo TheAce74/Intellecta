7cf0059534aee434bf145fa317e0337e6198ec36
Chisom Udonsi <udonsichisom02@gmail.com>
2025-01-31 05:06:06 -0500

    fix: fix deployment error

--- client/src/views/CourseGenerator.vue
+++ client/src/views/CourseGenerator.vue
@@ -35,7 +35,7 @@
           id="length"
           class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
           min="1"
-          max="10"
+          max="5"
           required
         />
       </div>
